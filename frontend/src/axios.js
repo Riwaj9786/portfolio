2673529ce05425f18d25e1127e3f6fa15e58e14e
Riwaj Bhurtel <riwajbhurtel@192.168.1.10>
2025-03-29 11:12:35 +0545

    Changes

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-   baseURL: 'http://192.168.1.18:8000/api/v1/',
+   baseURL: 'http://127.0.0.1:8000/api/v1/',
+   // baseURL: 'http://192.168.1.18:8000/api/v1/',
    // baseURL: 'http://172.16.100.81:8000/api/v1/',
    headers: {
       'Content-Type': 'application/json',
--- frontend/src/components/ContactForm.vue
+++ frontend/src/components/ContactForm.vue
@@ -22,17 +22,24 @@ const submitForm = async () => {
    try{
       const response = await axiosInstance.post("/contact/message/create/", formData.value);
 
-      msg.value = "Form Submitted successfully!";
-      msgClass.value = "bg-green-300"
+      if (response.status === 200 || response.status === 201) {
+         msg.value = "Form Submitted successfully!";
+         msgClass.value = "bg-green-300"
 
-      formData.value = {
-         name: "",
-         email: "",
-         message: ""
-      };
+         formData.value = {
+            name: "",
+            email: "",
+            message: ""
+         };
+      } else {
+         msg.value = "Failed to Submit Form."
+         msgClass.value = "bg-red-300"
+      }
+
    } catch (error) {
       console.error("Error submitting Form:", error);
       msg.value = "Failed to Submit Form.";
+      msgClass.value = "bg-red-500/50"
    } finally {
       loading.value = false
    }
@@ -45,6 +52,7 @@ const submitForm = async () => {
          Get in Touch:
       </div>
       <hr class="text-white">
+      <p v-if="msg" class="mt-3 text-gray-500 rounded-xl px-4 py-2" :class="msgClass">{{ msg }}</p>
       <form @submit.prevent="submitForm">
          <div class="w-full">
             <input type="text" v-model="formData.name" class="form-control mt-4 md:mt-8 w-full p-4 border border-gray-500 text-white rounded-xl" placeholder="Your Name">
@@ -61,6 +69,5 @@ const submitForm = async () => {
             </button>
          </div>
       </form>
-      <p v-if="msg" class="mt-3 text-gray-500 rounded-xl px-4 py-2" :class="msgClass">{{ msg }}</p>
    </div>
 </template>
