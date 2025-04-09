9be0095db02b390af7436068b4b14c1fe3706b32
Riwaj Bhurtel <riwajbhurtel@Riwajs-MacBook-Pro.local>
2025-04-09 13:42:02 +0545

    Updated Axios URL

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
    // baseURL: 'http://127.0.0.1:8000/api/v1/',
-   baseURL: 'http://192.168.1.7:8000/api/v1/',
-   // baseURL: 'http://172.16.100.81:8000/api/v1/',
+   // baseURL: 'http://192.168.1.7:8000/api/v1/',
+   baseURL: 'http://172.16.100.81:8000/api/v1/',
    headers: {
       'Content-Type': 'application/json',
    },
