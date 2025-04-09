cc1d1a20f9b7e0d268b3095eb127b2ad85df23ea
Riwaj Bhurtel <riwajbhurtel@Riwajs-MacBook-Pro.local>
2025-04-09 16:36:21 +0545

    Deployment ready

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const axiosInstance = axios.create({
    // baseURL: 'http://127.0.0.1:8000/api/v1/',
    // baseURL: 'http://192.168.1.7:8000/api/v1/',
-   baseURL: 'http://172.16.100.81:8000/api/v1/',
+   // baseURL: 'http://172.16.100.81:8000/api/v1/',
+   baseURL: 'https://www.riwajbhurtel.bbtnepal.com/api/v1/',
    headers: {
       'Content-Type': 'application/json',
    },
