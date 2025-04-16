bac0ea3a52a2186c5d97648a3e47b8451bd58c52
Riwaj Bhurtel <riwajbhurtel@192.168.1.6>
2025-04-16 20:56:30 +0545

    Updated api base

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,7 +11,9 @@ urlpatterns = [
     path('admin/password_change/done/', auth_views.PasswordResetDoneView.as_view(), name='password_change_done'),
     path('admin/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_change_confirm'),
     path('admin/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_change_complete'),
-
+    
+    path('api/', lambda request: JsonResponse({"message": "Welcome to the API root"})),
+    
     path('api/v1/information/', include('account.urls')),
     path('api/v1/experience/', include('experience.urls')),
     path('api/v1/skill/', include('skills.urls')),
